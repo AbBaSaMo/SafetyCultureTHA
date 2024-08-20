740d953a6741055cb7bba60ffdb864023142dc9f
Baaset Moslih <s.a.baaset.moslih@gmail.com>
2024-08-21 01:00:24 +1000

    tests: implemented final test case

--- folders/folders_test.go
+++ folders/folders_test.go
@@ -2,19 +2,11 @@ package folders
 
 import (
 	"errors"
+	"reflect"
 	"testing"
 
 	"github.com/gofrs/uuid"
 )
-
-// res: &FetchFolderResponse{Folders: []*Folder{
-// 	{
-// 		Id:      uuid.FromStringOrNil("1167c1ac-911b-4a1f-b460-a98f724c7289"),
-// 		Name:    "heroic-bella",
-// 		OrgId:   uuid.FromStringOrNil("4212d618-66ff-468a-862d-ea49fef5e183"),
-// 		Deleted: true,
-// 	},
-// }},
 
 func Test_GetAllFolders(t *testing.T) {
 	t.Run("test", func(t *testing.T) {
@@ -38,8 +30,16 @@ func Test_GetAllFolders(t *testing.T) {
 
 		// existant org uuid
 		res, err = GetAllFolders(&FetchFolderRequest{OrgID: uuid.FromStringOrNil("4212d618-66ff-468a-862d-ea49fef5e183")})
-		if err != nil {
-			t.Errorf("got res %v err %v expected res %v err %v", *res, err, FetchFolderResponse{Folders: []*Folder{}}, nil)
+		expectedRes := &FetchFolderResponse{Folders: []*Folder{
+			{
+				Id:      uuid.FromStringOrNil("1167c1ac-911b-4a1f-b460-a98f724c7289"),
+				Name:    "heroic-bella",
+				OrgId:   uuid.FromStringOrNil("4212d618-66ff-468a-862d-ea49fef5e183"),
+				Deleted: true,
+			},
+		}}
+		if !reflect.DeepEqual(res, expectedRes) || err != nil {
+			t.Errorf("got res %+v err %v expected res %+v err %v", res, err, expectedRes, nil)
 		}
 	})
 }
